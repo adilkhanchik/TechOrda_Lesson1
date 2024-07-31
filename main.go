f975a30a5ecf62931046a845e9e8b8a13fed256a
adilkhanchik <aid.adi@mail.ru>
2024-07-31 14:12:13 +0500

    test

--- main.go
+++ main.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 )
 
-// sumUpTo takes an integer n and returns the sum of all numbers from 1 to n.
 func sumUpTo(n int) int {
-	// Initialize a variable to hold the sum
 	sum := 0
 
-	// Loop from 1 to n, adding each number to sum
 	for i := 1; i <= n; i++ {
 		sum += i
 	}
@@ -18,7 +15,6 @@ func sumUpTo(n int) int {
 }
 
 func main() {
-	// Test the function with an example input
 	result := sumUpTo(4)
-	fmt.Println(result) // Output: 10
+	fmt.Println(result)
 }
